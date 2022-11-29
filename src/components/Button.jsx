cea61059e706c60969010f041b3cc1ecd83d3668
Melbine <melbineyt@gmail.com>
2022-11-29 19:22:37 +0300

    end project

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styLes }) => {
+  return (
+    <button
+      type="button"
+      className={`  rounded-[10px] py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styLes}`}>
+      Get Started
+    </button>
+  );
 };
 
 export default Button;
